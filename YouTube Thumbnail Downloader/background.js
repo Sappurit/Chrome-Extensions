fe64253597433977b302150fe52a4cf0fba3cf4d
Sappurit <42336301+Sappurit@users.noreply.github.com>
2020-07-22 00:13:17 +0700

    Add files via upload

--- YouTube Thumbnail Downloader/background.js
+++ YouTube Thumbnail Downloader/background.js
@@ -1,16 +1,18 @@
 chrome.browserAction.onClicked.addListener(function() {
 	chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-		if (tabs[0].url.indexOf("youtube.com/watch?v=") == -1) {
-			alert('You must stay on YouTube video page.\nE.g. https://www.youtube.com/watch?v=kJQP7kiw5Fk');
-		} else {
+		if (tabs[0].url.includes('youtube.com/watch?v=')) {
+			let filename  = tabs[0].title.replace(/ - YouTube$/, '') + '.jpg';
 			let youTubeId = tabs[0].url.split(/=|&/)[1];
-			let url1 = "https://img.youtube.com/vi/" + youTubeId + "/maxresdefault.jpg";
-			let url2 = "https://img.youtube.com/vi/" + youTubeId + "/hqdefault.jpg";
+			let url1      = 'https://img.youtube.com/vi/' + youTubeId + '/maxresdefault.jpg';
+			let url2      = 'https://img.youtube.com/vi/' + youTubeId + '/hqdefault.jpg';
 
-			chrome.downloads.download({ url: url1, filename: youTubeId + ".jpg", saveAs: true }, function(downloadId) {
+			chrome.downloads.download({ url: url1, filename: filename, saveAs: true }, function(downloadId) {
 	                        if (typeof downloadId === 'number') { return; }
-				chrome.downloads.download({ url: url2, filename: youTubeId + ".jpg", saveAs: true });
+				chrome.downloads.download({ url: url2, filename: filename, saveAs: true });
 			});
+
+		} else {
+			alert('You must stay on YouTube video page.\nE.g. https://www.youtube.com/watch?v=kJQP7kiw5Fk');
                 }
 	});
 });
