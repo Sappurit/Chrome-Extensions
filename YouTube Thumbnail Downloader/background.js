c519e3e45b93f102cb195e6067b128384af41552
Sappurit <42336301+Sappurit@users.noreply.github.com>
2020-07-21 23:02:48 +0700

    Add files via upload

--- YouTube Thumbnail Downloader/background.js
+++ YouTube Thumbnail Downloader/background.js
@@ -1,6 +1,6 @@
-chrome.browserAction.onClicked.addListener(function(activeTab) {
+chrome.browserAction.onClicked.addListener(function() {
 	chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-		if (tabs[0].url.indexOf("www.youtube.com/watch?v=") == -1) {
+		if (tabs[0].url.indexOf("youtube.com/watch?v=") == -1) {
 			alert('You must stay on YouTube video page.\nE.g. https://www.youtube.com/watch?v=kJQP7kiw5Fk');
 		} else {
 			let youTubeId = tabs[0].url.split(/=|&/)[1];
@@ -8,17 +8,8 @@ chrome.browserAction.onClicked.addListener(function(activeTab) {
 			let url2 = "https://img.youtube.com/vi/" + youTubeId + "/hqdefault.jpg";
 
 			chrome.downloads.download({ url: url1, filename: youTubeId + ".jpg", saveAs: true }, function(downloadId) {
-	                        if (typeof downloadId === "integer")
-	                        {
-					return;
-	                        }
-			});
-
-			chrome.downloads.download({ url: url2, filename: youTubeId + ".jpg", saveAs: true }, function(downloadId) {
-	                        if (typeof downloadId === "integer")
-	                        {
-					return;
-	                        }
+	                        if (typeof downloadId === 'number') { return; }
+				chrome.downloads.download({ url: url2, filename: youTubeId + ".jpg", saveAs: true });
 			});
                 }
 	});
