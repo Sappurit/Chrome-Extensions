51d70dd274b4a895df17277e503cecb10dd8281c
Sappurit <42336301+Sappurit@users.noreply.github.com>
2020-07-21 21:28:02 +0700

    Add files via upload

---
+++ YouTube Thumbnail Downloader/background.js
@@ -0,0 +1,25 @@
+chrome.browserAction.onClicked.addListener(function(activeTab) {
+	chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+		if (tabs[0].url.indexOf("www.youtube.com/watch?v=") == -1) {
+			alert('You must stay on YouTube video page.\nE.g. https://www.youtube.com/watch?v=kJQP7kiw5Fk');
+		} else {
+			let youTubeId = tabs[0].url.split(/=|&/)[1];
+			let url1 = "https://img.youtube.com/vi/" + youTubeId + "/maxresdefault.jpg";
+			let url2 = "https://img.youtube.com/vi/" + youTubeId + "/hqdefault.jpg";
+
+			chrome.downloads.download({ url: url1, filename: youTubeId + ".jpg", saveAs: true }, function(downloadId) {
+	                        if (typeof downloadId === "integer")
+	                        {
+					return;
+	                        }
+			});
+
+			chrome.downloads.download({ url: url2, filename: youTubeId + ".jpg", saveAs: true }, function(downloadId) {
+	                        if (typeof downloadId === "integer")
+	                        {
+					return;
+	                        }
+			});
+                }
+	});
+});
